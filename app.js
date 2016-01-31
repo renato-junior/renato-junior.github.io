8d3277f39fbce98ddcc4a86d886d37d0fc6d7313
renato287 <renatosergiolopez@hotmail.com>
2016-01-31 14:11:22 -0200

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module('routeApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  
+  $routeProvider.when("/", 
+    {
+      templateUrl: "inicial.html"
+    }
+  )
+  .when("/academic", 
+    {
+      templateUrl: "academico.html"
+    }
+  )
+  .when("/websites", 
+    {
+      templateUrl: "websites.html"
+    }
+  )
+  .when("/apps", 
+    {
+      templateUrl: "apps.html"
+    }
+  )
+  .otherwise(
+    {
+      redirectTo: "/"    
+    }
+  );
+});
